b1d32e0cd0d438096b11124f822d474df9ff04d8
zichunguo <guozichunvip@163.com>
2020-06-18 10:41:01 +0800

    添加 zuul 路由网关

---
+++ mycloud-zuul-gateway-9527/src/main/java/com/cloud/test/Zuul9527_App.java
@@ -0,0 +1,15 @@
+package com.cloud.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul9527_App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zuul9527_App.class, args);
+	}
+
+}
