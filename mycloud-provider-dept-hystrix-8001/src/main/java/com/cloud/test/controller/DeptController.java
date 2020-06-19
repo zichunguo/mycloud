bc8795cb59451eb9a2ca58423ab68feba142d646
zichunguo <guozichunvip@163.com>
2020-06-19 13:21:04 +0800

    修改完善

--- mycloud-provider-dept-hystrix-8001/src/main/java/com/cloud/test/controller/DeptController.java
+++ mycloud-provider-dept-hystrix-8001/src/main/java/com/cloud/test/controller/DeptController.java
@@ -36,6 +36,7 @@ public class DeptController {
 		return dept;
 	}
 	
+	// 服务熔断后的处理方法
 	public Dept processHystrix_Get(@PathVariable("id") Long id) {
 		return new Dept().setDeptno(id).setDname("该ID：" + id + "没有对应信息, null--@HystrixCommand")
 				.setDb_source("no this database in MySQL.");
